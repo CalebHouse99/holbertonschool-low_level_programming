4a28db3c687460560fb438972e66ff0eeab7d5d9
CalebHouse99 <calebhouse99@gmail.com>
2022-05-20 14:47:54 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
+/*
+ *main - Print a string
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
- return (0);
+return (0);
 }
