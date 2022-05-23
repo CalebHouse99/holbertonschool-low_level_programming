3dbc82def1b76181c8e037b39ed0403144192986
CalebHouse99 <calebhouse99@gmail.com>
2022-05-23 10:58:33 -0700

    a

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,13 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("$n is positive\n");
-} else if (n == 0)
+}
+else if (n == 0)
 {
 printf("$n is zero\n");
 else
 {
 printf("$n is negative\n")
-return(0);
+}
+return (0);
 }
