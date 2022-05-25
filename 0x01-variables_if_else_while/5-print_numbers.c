e9cd64ebe08c48abde88ac737ef59741764513cb
CalebHouse99 <calebhouse99@gmail.com>
2022-05-25 10:03:52 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - count from 1-10
+*
+* Return: 0
+*/
+int main(void)
+{
+int i;
+char ch = "0123456789";
+for (i = 0; i < 10; i++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
