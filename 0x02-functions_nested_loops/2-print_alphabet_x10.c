66b4114a8b36288750edddbd9e62ad5938a34eac
CalebHouse99 <calebhouse99@gmail.com>
2022-05-24 21:01:22 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_alphabet_x10 - Print the alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char s[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (n = 0; n < 10; n++)
+{
+for (i = 0; i < 26; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
+}
