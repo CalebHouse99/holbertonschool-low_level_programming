1112314df9a64b8d0fc1907f35bc962c7c4984ad
CalebHouse99 <calebhouse99@gmail.com>
2022-05-25 08:57:19 -0700

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,23 @@
  */
 int main(void)
 {
-  int n;
-  int v;
-  
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  v = n % 10;
-  if (v > 5)
-    {
-      printf("%d and is greater than 5", v);
-    }
-  else if (v == 0)
-    {
-      printf("%d and is 0", v);
-    }
-  else
-    {
-      printf("%d and is less than 6 and not 0", v)
-    }
+int n;
+int v;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+v = n % 10;
+if (v > 5)
+{
+printf("%d and is greater than 5", v);
+}
+else if (v == 0)
+{
+printf("%d and is 0", v);
+}
+else
+{
+printf("%d and is less than 6 and not 0", v);
+}
 return (0);
 }
