db7059cc1a2178fa66e3ca6973b77bc349bc4ba1
CalebHouse99 <calebhouse99@gmail.com>
2022-05-25 20:04:41 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24-hours.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+void jack_bauer(void);
+{
+int hour;
+int minute;
+
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+_putchar('0' + hour / 10);
+_putchar('0' + hour % 10);
+_putchar(':');
+_putchar('0' + second / 10);
+_putchar('0' + second % 10);
+}
+}
+}
