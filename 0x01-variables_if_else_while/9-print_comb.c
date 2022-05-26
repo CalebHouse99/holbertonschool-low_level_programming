cc905c6452007aca731d7e92ad0fc48d96c43ac6
CalebHouse99 <calebhouse99@gmail.com>
2022-05-25 18:59:31 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - print numbers of base 16
+*
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
