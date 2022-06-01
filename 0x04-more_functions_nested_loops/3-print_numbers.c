38bf5ba3712727c454743f691f9fbb250908bd95
CalebHouse99 <calebhouse99@gmail.com>
2022-05-31 18:04:47 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return : Always 0
+ */
+void print_numbers(void)
+{
+char ch[] = "0123456789";
+
+for (i = 1; i < 11; i++)
+{
+_putchar(ch[i]);
+}
+_putchar('\n');
+return (0);
+}
