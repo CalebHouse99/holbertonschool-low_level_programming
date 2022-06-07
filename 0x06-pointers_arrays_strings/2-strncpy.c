ea3d9f3084aa407686d146724b0d6f9ea1aea6ed
CalebHouse99 <calebhouse99@gmail.com>
2022-06-06 21:44:57 -0700

    a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strncpy - dest combines with src
+ * @dest: first string
+ * @src: second string
+ * @n: variable
+ * Return: 0;
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int a = 0;
+
+while (a < n && src[a] != '\0')
+{
+dest[a] = src[a];
+a++;
+}
+while (b < n)
+{
+dest[a] = '\0';
+a++;
+}
+return (dest);
+}
