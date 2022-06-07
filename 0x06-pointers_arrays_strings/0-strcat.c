ec9efccec945f94d15bbb61930bec94c53844c9e
CalebHouse99 <calebhouse99@gmail.com>
2022-06-06 20:20:42 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,12 @@
 #include "main.h"
 /**
  * _strcat - dest combines with src
- *
+ * @dest: first string
+ * @src: second string
  * Return: 0;
  */
 char *_strcat(char *dest, char *src)
 {
-_strcat(dest,src);
+_strcat(dest, src);
 return (0);
 }
