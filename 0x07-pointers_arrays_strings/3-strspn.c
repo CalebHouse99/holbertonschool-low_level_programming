6d55ae5a1422b80c56bf964203873910efd50ee4
CalebHouse99 <calebhouse99@gmail.com>
2022-06-07 21:31:11 -0700

    a

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * x - the function
+ *
+ * Return: 0
+ */
+
+{
+return (0);
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard
+ * @a: pointer
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+int b = 0;
+int c = 0;
+while (c < 8)
+{
+_putchar(a[c][b]);
+x++;
+}
+c++;
+_putchar('\n');
+}
+}
