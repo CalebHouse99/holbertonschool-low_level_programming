14e7f3ea915a7d874b98b1fb79f94de3c71ba860
CalebHouse99 <calebhouse99@gmail.com>
2022-06-07 20:10:08 -0700

    a

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - replaces characters
+ *@dest: destination
+ *@b: filler
+ *@n: number of bytes
+ * Return: 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+for (a = 0; a < n; a++)
+{
+dest[a] = src[a];
+}
+return (s);
+}
