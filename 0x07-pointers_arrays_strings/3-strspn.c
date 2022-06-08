f2db9b1a6f239969e546bb07e62f031e0b09c937
CalebHouse99 <calebhouse99@gmail.com>
2022-06-07 21:56:12 -0700

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,4 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int n = 0;
-int a, b;
-
-a = 0;
-
-while (s[a] != '\0')
-{
-for (b = 0; accept[b] != '\0'; b++)
-{
-if (s[a] == accept[b])
-{
-count++;
-break;
-}
-if (accept[b] == '\0')
-{
-break;
-a++;
-}
-}
-return (n);
 }
