3988c1bff0fee4cde2d63c47f21d59e444b2664e
CalebHouse99 <calebhouse99@gmail.com>
2022-06-07 21:40:02 -0700

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,32 @@
 #include "main.h"
 /**
- * x - the function
+ * _strspn - the function
  *
- * Return: 0
+ * @s: string
+ * @accept: result
+ *
+ * Return: n
  */
-
+unsigned int _strspn(char *s, char *accept)
 {
-return (0);
+unsigned int n = 0;
+int a, b;
+
+a = 0;
+
+while (s[a] != '\0')
+{
+for (b = 0; accept[b] != '\0'; b++)
+{
+if (s[a] == accept[b])
+{
+count++;
+break;
+}
+}
+if (accept[b] == '\0')
+break;
+a++;
+}
+return (count);
 }
