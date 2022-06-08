113d9a8fdd4c8cc1eeec53bbae8ab6dee1e9ad0d
CalebHouse99 <calebhouse99@gmail.com>
2022-06-07 20:39:00 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strchr - replaces characters
+ *@s: first occurrence
+ *@c: of this
+ * Return: first character
+ */
+char *_strchr(char *s, char c);
+{
+unsigned int a;
+for (a = 0; a == c; a++)
+{
+c[s] = c[a];
+}
+return (s);
+}
