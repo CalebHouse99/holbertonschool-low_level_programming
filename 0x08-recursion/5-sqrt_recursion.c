161761aced27c4c26484b5034f36c66623a092cc
CalebHouse99 <calebhouse99@gmail.com>
2022-06-08 21:52:14 -0700

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * find_square - finds the square
+ *
+ *
+ *
+ */
+int find_square(int n, int a)
+{
+if (a * a > n)
+return (-1);
+
+if (a * a == n)
+return (a);
+
+return (squirt_r(n, a + 1));
+}
+
+/**
+ * _sqrt_recursion - function
+ *
+ *@n: number
+ *
+ *
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (squirt_r(n, 0));
+}
+}
