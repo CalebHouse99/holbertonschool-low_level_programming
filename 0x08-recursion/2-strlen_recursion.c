776a68152266b8c924d62ff06503c9d991ed73f8
CalebHouse99 <calebhouse99@gmail.com>
2022-06-08 21:05:45 -0700

    a

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,12 @@
  */
 int _strlen_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-return (1 + _strlen_recursion(s++));
+return (0);
 }
 else
 {
-return (0);
+return (1 + _strlen_recursion(s + 1));
 }
 }
