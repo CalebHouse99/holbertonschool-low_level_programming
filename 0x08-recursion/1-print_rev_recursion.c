794aa0558677cc203123348f1e105d06c2826870
CalebHouse99 <calebhouse99@gmail.com>
2022-06-08 17:55:54 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - like putchar, but no loops!
+ *
+ *
+ *@s: string
+ *
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_puts_recursion(s);
+s++;
+_putchar(*s);
+}
+else
+{
+_putchar('\n');
+}
+}
