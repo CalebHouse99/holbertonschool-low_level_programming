daa72513903ed006eb369f165e6f0f5e684d9b35
CalebHouse99 <calebhouse99@gmail.com>
2022-06-09 21:03:01 -0700

    a

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capilalize all words in a string
+ * @s: input string
+ *
+ * Return: cap
+ */
+char *cap_string(char *s)
+{
+int i = 0;
+
+while (s[i])
+{
+if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+{
+s[i] -= 32;
+}
+switch (s[i])
+{
+case ' ':
+case '\t':
+case '\n':
+case ',':
+case ';':
+case '.':
+case '!':
+case '?':
+case '"':
+case '(':
+case ')':
+case '{':
+case '}':
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+{
+s[i + 1] -= 32;
+}
+}
+i++;
+}
+return (s);
+}
