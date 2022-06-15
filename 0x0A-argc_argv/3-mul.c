6f4a05b628247d8b621aeef49c1f741fb5b99ba4
CalebHouse99 <calebhouse99@gmail.com>
2022-06-14 21:18:05 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,16 +5,22 @@
  * main - function
  * @argc: number
  * @argv: string
- * Return: Always 0
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char  *argv[])
 {
-if (argc != 3)
+int i;
+
+if (argc == 3)
+{
+i = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", i);
+return (0);
+}
+else
 {
 printf("Error\n");
 return (1);
 }
-printf("%d\n", (atoi(argv[1])) * (atoi(argv[2])));
-return (0);
 }
