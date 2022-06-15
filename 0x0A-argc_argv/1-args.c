135a7933efb99476128453ff09248cffac280293
CalebHouse99 <calebhouse99@gmail.com>
2022-06-14 21:15:28 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: number
+ * @argv: string
+ * Return: 0
+ */
+
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+if (argc > 0)
+{
+printf("%d\n", argc - 1);
+}
+return (0);
+}
