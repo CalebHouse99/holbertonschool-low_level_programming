7217f3b959d35336669a65557ea1cd56e63d837f
CalebHouse99 <calebhouse99@gmail.com>
2022-06-20 08:23:19 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,36 +1,53 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * str_concat - concatenate strings
+ * str_concat - Concatenates two strings of any size
+ * @s1: the first string to concatenate
+ * @s2: the second string to concatenate
  *
- *@s1: string one
- *@s2: string two
- *
- * Return: concatonated string
+ * Return: the two strings concatenated
  */
-
 char *str_concat(char *s1, char *s2)
 {
-int i = 0;
-int j = 0;
+int i = 0, j = 0, k = 0, l = 0;
+char *s;
 
 if (s1 == NULL)
-s1 = " ";
+s1 = "";
+
 if (s2 == NULL)
-s2 = " ";
+s2 = "";
 
-char *a = malloc(sizeof(char) * (strlen(s1) + strlen(s2)));
+while (s1[i])
+i++;
 
-while (s1)
+while (s2[j])
+j++;
+
+l = i + j;
+s = malloc((sizeof(char) * l) + 1);
+
+if (s == NULL)
+return (NULL);
+
+j = 0;
+
+while (k < l)
 {
-a[i] = s[i];
-i++;
-}
-while (s2)
+if (k <= i)
+s[k] = s1[k];
+
+if (k >= i)
 {
-a[i] = s2[i];
-i++;
+s[k] = s2[j];
 j++;
 }
+
+k++;
+}
+
+s[k] = '\0';
+return (s);
 }
