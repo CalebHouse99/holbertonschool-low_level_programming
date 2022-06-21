c7b2f8ab5f66c2a7539b30161800d2fe2e26ee50
CalebHouse99 <calebhouse99@gmail.com>
2022-06-21 16:56:50 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,54 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * string_nconcat - concatonates bytes onto a string
- * @s1: string 1
- * @s2: string 2
- * @n: bytes
+ * string_nconcat - concatenates two strings
+ *
+ * @s1: string 1 to be cat
+ * @s2: string 2 to be cat
+ * @n: byte limit of str 2
+ *
  * Return: pointer
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int s1len, s2len, i;
-char *s;
+unsigned int i = 0, l1 = 0, l2 = 0, len = 0;
+char *str;
+char *junk;
+
+junk = malloc(5);
+free(junk);
 
 if (s1 == NULL)
 s1 = "";
-
 if (s2 == NULL)
 s2 = "";
 
-s1len = strlen(s1);
-s2len = strlen(s2);
+l1 = strlen(s1);
+l2 = strlen(s2);
 
-if ((int) n <= s2len)
-s2len = n;
+if (n >= l2)
+len = l1 + l2;
+else
+len = l1 + n;
 
-s = malloc(s1len + s2len + 1);
-if (s == NULL)
+str = malloc(sizeof(char) * len + 1);
+if (str == NULL)
 return (NULL);
 
-i = 0;
-while (i < s1len)
+l2 = 0;
+while (i < len)
 {
-s[i] = s1[i];
+if (i <= l1)
+str[i] = s1[i];
+
+if (i >= l1)
+{
+str[i] = s2[l2];
+l2++;
+}
 i++;
 }
-i = 0;
-while (i < s2len)
-{
-s[i + s1len] = s2[i];
-i++;
-}
-
-s[s1len + s2len] = '\0';
-return (s);
-
+str[i] = '\0';
+return (str);
 }
