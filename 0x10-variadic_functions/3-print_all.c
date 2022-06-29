ee0bc7aa2f8c675ec2bc9b1d67e3b0891e22a7be
CalebHouse99 <calebhouse99@gmail.com>
2022-06-28 21:51:34 -0700

    basepoint

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+
 /**
- * print_strings -
- * @n: number of numbers
+ * print_all - prints everything!
+ * @format: any and every format
  *
- * Return:
  */
 
 void print_all(const char * const format, ...)
 {
+
 }
