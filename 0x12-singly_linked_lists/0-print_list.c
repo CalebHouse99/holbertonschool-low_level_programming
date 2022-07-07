728a626603c4834f609974f9773be3fc43b7764e
CalebHouse99 <calebhouse99@gmail.com>
2022-07-07 09:31:12 -0700

    a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include <stdlib>
+#include <stdio.h>
+
+/**
+* print_list - prints all elements of list_t
+*
+* list_t - list to print
+* h - pointer to the first node
+*
+* Return: nodeCount
+*/
+
+size_t print_list(const list_t *h)
+{
+  int nodeCount;
+
+}
