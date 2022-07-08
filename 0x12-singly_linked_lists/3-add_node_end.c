a7b0cb62dfdd745ed5da82dd4bff68c78ac42706
CalebHouse99 <calebhouse99@gmail.com>
2022-07-08 10:21:05 -0700

    at least it prints one

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,6 +16,27 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	struct list_s *new_node;
+	struct list_s *last_node = *head;
 
-	
+	new_node = malloc(sizeof(struct list_s));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		{
+		*head = new_node;
+		return (new_node);
+		}
+	else	
+		{
+			while(last_node->next != NULL)
+			{
+				last_node = last_node->next;
+				last_node->next = new_node;
+			}
+		}
+	return (0);
 }
