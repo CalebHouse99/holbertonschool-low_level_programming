352bc77bea5cf3627aa7bd8390ba40fb1de7089a
CalebHouse99 <calebhouse99@gmail.com>
2022-07-08 06:33:37 -0700

    first 2 commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* add_node - function that adds a new node at the beginning of list_t
+*
+* @head: pointer to the first node
+*
+* @str: first element of the node
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	strdup(str);
+	struct list_t * new_node = malloc(sizeof(struct list_s));
+	new_node->next = str;
+}
