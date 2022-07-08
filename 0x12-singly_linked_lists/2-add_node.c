22217e528c9775c549dfc3149194e5c5f1a0729a
CalebHouse99 <calebhouse99@gmail.com>
2022-07-08 09:51:28 -0700

    NULL check

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,6 +18,8 @@ list_t *add_node(list_t **head, const char *str)
 	struct list_s *new_node;
 
 	new_node = malloc(sizeof(struct list_s));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
