e3e8dcdb984ddd34a206ff41acb366a9490196af
CalebHouse99 <calebhouse99@gmail.com>
2022-07-08 11:52:17 -0700

    never mind, it does not work

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,12 +12,14 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *temp = head;
 
 	while (head)
 	{
 		temp = head;
 		head = head->next;
+		free(temp->str);
 		free(temp);
 	}
+	free (head);
 }
