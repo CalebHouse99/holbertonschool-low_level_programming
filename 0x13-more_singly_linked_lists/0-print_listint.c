4011f4c756a77c825cd3c68cacaf6c2d3b5aea97
CalebHouse99 <calebhouse99@gmail.com>
2022-07-11 09:31:05 -0700

    fixed 0 to make 2 work

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -17,7 +17,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		if (h->n != 0)
+		if (h->n >= 0)
 		{
 			printf("%d\n", h->n);
 		}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* add_nodeint - function that adds a new node at the beginning of listint_t
+*
+* @head: pointer to the first node
+*
+* @str: first element of the node
+*
+* Return: NULL or address to new node
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	struct listint_s *new_node;
+
+	new_node = malloc(sizeof(struct listint_s));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	if (n != 0)
+		return (new_node);
+	else
+		return (0);
+}
