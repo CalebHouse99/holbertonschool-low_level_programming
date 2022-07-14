2c805d09dd43f8de5768bbbb5f909f7dadd15b42
CalebHouse99 <calebhouse99@gmail.com>
2022-07-13 18:46:53 -0700

    backwards!

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,21 +13,25 @@
 void print_binary(unsigned long int n)
 {
 	long converted = 0;
-	int remainder, temp = 1;
+	long remainder, temp = 1;
+
+	if (n == 0)
+		_putchar('0');
 
 	while (n != 0)
 	{
 		if (n & 1)
 		{
 			remainder = 1;
+			_putchar('1');
 		}
 		else
 		{
 			remainder = 0;
+			_putchar('0');
 		}
 		n = n >> 1;
 		converted = converted + (remainder * temp);
 		temp = temp * 10;
 	}
-	printf("%li", converted);
 }
