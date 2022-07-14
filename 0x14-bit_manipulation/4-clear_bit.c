e1b4d71106e36f08aa4fb04e4a280e9949ead190
CalebHouse99 <calebhouse99@gmail.com>
2022-07-13 20:22:32 -0700

    betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
+* clear_bit - sets value of bit to 0 at index
 *
+*@n: number
+*@index: index
 *
-*
-*
-*
+* Return: 1 on success, -1 on failure
 */
 
 int clear_bit(unsigned long int *n, unsigned int index)
@@ -13,5 +14,5 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	*n = 0;
 	index = 0;
 	index++;
-	return (0);
+	return (1);
 }
