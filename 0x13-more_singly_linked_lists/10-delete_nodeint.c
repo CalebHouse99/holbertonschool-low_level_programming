2bfe133af85c90b73c4d27e4621142ca885f293a
CalebHouse99 <calebhouse99@gmail.com>
2022-07-14 09:26:14 -0700

    return 1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,6 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = (*head)->next;
 		(*head)->next = temp->next;
 		free(temp);
+		return (1);
 	}
 
 	for (i = 0; i < counter; i++, iterator = iterator->next)
