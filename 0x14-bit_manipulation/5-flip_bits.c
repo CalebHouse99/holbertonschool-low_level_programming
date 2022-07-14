1a1016c7590910a574790342e4139f77b10dd8d7
CalebHouse99 <calebhouse99@gmail.com>
2022-07-13 20:26:54 -0700

    init commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* flip_bits - flips the bits
+*
+* @m: index
+* @n: number
+*
+* Return: 0
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	n = 0;
+	m = 0;
+	m++;
+	n++;
+	return (0);
+}
