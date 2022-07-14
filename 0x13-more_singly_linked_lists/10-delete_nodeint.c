9c16ca4d091addec00c58223a2434b697dd55aa0
CalebHouse99 <calebhouse99@gmail.com>
2022-07-14 09:16:14 -0700

    init commit

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* delete_nodeint_at_index - deletes node int at and index
+*
+* @head: points to first node
+* @index: given index to delete
+*
+* Return: 1 on success, -1 on failure
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	int counter = index - 1, i;
+	listint_t *temp, *iterator = *head;
+
+	for (i = 0; i < counter; i++, iterator = iterator->next)
+	{
+		if (!iterator->next)
+			return (-1);
+	}
+	temp = iterator->next;
+	iterator->next = temp->next;
+	free(temp);
+	return (1);
+}
