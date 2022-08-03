6a88e89cf515343e746231edccebd376c0b51395
CalebHouse99 <calebhouse99@gmail.com>
2022-08-02 21:27:02 -0700

    init commit

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,11 @@
+/**
+ * print_dlistint - Print all elements of doubly linked list
+ * @h: pointer to the head
+ * 
+ * Return: # of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	
+}
