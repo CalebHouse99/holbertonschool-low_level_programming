8df7bcd45f558c9d8a0356cb70375e379791d791
CalebHouse99 <calebhouse99@gmail.com>
2022-08-02 21:31:07 -0700

    betty

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,26 @@
 /**
  * print_dlistint - Print all elements of doubly linked list
  * @h: pointer to the head
- * 
+ *
  * Return: # of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	
+	size_t nodeCount = 0;
+
+	while (h)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+			h = h->next;
+			nodeCount++;
+	}
+	return (nodeCount);
 }
