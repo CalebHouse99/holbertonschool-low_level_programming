e0a76ff439e677490128fc0151405eb2465bf097
CalebHouse99 <calebhouse99@gmail.com>
2022-08-05 11:48:05 -0700

    task 4

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -1,3 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "lists.h"
 
+/**
+* free_dlistint - frees the list
+*
+* @head: pointer to the first node
+*
+*/
+
 void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->n);
+		free(temp);
+	}
+}
