b8182f0329c52d35247c6f822acd8f37b215e846
CalebHouse99 <calebhouse99@gmail.com>
2022-08-05 09:06:29 -0700

    number 3

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds node at end of list
+ *
+ * @head: pointer to head node
+ * @n: value
+ * Return: node address
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t temp = *head;
+	dlistint_t *new = malloc(sizeof(new) + 1);
+
+	if (new == NULL)
+		return (NULL);
+	
+	new->n = n;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+	new->prev = temp;
+	new->next = NULL;
+	return (new); 
+}
---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_backward - prints the doubly linked list backwards
+ *
+ * @h: pointer to head
+ * Return: # of nodes
+ */
+
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+
+}
