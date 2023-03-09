9fa7754889c23aabfdbf7087d92eb32501c79cbe
unknown <calebhouse99@gmail.com>
2023-03-08 20:35:55 -0600

    edge cases

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -15,39 +15,63 @@ int advanced_binary(int *array, size_t size, int value)
     if (array == NULL || size == 0)
         return (-1);
 
-    return advanced_binary_helper(array, value, 0, size - 1);
+    int low = 0, high = size - 1, mid, index = -1;
+    while (low <= high)
+    {
+        printf("Searching in array: ");
+        for (int i = low; i <= high; i++)
+            printf("%d%s", array[i], i == high ? "\n" : ", ");
+
+        mid = (low + high) / 2;
+        if (array[mid] < value)
+            low = mid + 1;
+        else if (array[mid] > value)
+            high = mid - 1;
+        else
+        {
+            index = mid;
+            break;
+        }
+    }
+
+    if (index == -1)
+        return index;
+
+    // Check if the value occurs only once in the array
+    if (index == 0 || array[index - 1] != value)
+        return index;
+
+    // Search left of the mid for the first occurrence of the value
+    return advanced_binary_helper(array, value, low, index);
 }
 
 /**
- * advanced_binary_helper - Searches for a value in a sorted array of integers.
+ * advanced_binary_helper - Searches for the first occurrence of a value in a sorted array of integers.
  * @array: A pointer to the first element of the array to search in.
  * @value: The value to search for.
  * @start: The starting index of the subarray to search in.
  * @end: The ending index of the subarray to search in.
  *
- * Return: The index where value is located, or -1 if not found.
+ * Return: The index where the first occurrence of value is located, or -1 if not found.
  */
 int advanced_binary_helper(int *array, int value, int start, int end)
 {
-    int mid, i;
+    int mid;
+    while (start < end)
+    {
+        printf("Searching in array: ");
+        for (int i = start; i <= end; i++)
+            printf("%d%s", array[i], i == end ? "\n" : ", ");
 
-    printf("Searching in array: ");
-    for (i = start; i <= end; i++)
-        printf("%d%s", array[i], i == end ? "\n" : ", ");
+        mid = (start + end) / 2;
+        if (array[mid] < value)
+            start = mid + 1;
+        else if (array[mid] >= value)
+            end = mid;
+    }
 
-    if (start == end)
-        return (array[start] == value ? start : -1);
+    if (array[start] == value)
+        return start;
 
-    mid = (start + end) / 2;
-
-    if (array[mid] < value)
-        return advanced_binary_helper(array, value, mid + 1, end);
-
-    if (array[mid] > value)
-        return advanced_binary_helper(array, value, start, mid - 1);
-
-    if (mid != start && array[mid - 1] == value)
-        return advanced_binary_helper(array, value, start, mid);
-
-    return mid;
+    return -1;
 }
